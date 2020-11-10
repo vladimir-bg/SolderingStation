979c335f82321fbf39e77ae748b3e5c6fe5517f9
vladimir-bg <vladimirselezniov@gmail.com>
2020-11-10 20:20:59 +0200

    Making first tests

--- SolderingStation/src/main.cpp
+++ SolderingStation/src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#define termoCouple A3
+#define A0 ambientCalibration
+uint8_t heater = 10;
+uint16_t temp;
+uint8_t tempDealay = 10;
+long timePass = millis();
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(heater, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(timePass - millis() == tempDealay) {
+    Serial.println(tempRead());
+  }
+  
+  timePass = millis();
+}
+
+float tempRead() {
+  uint16_t temp;
+  for(int tempFilter = 0; tempFilter < 10; tempFilter++) {
+    temp += analogRead(termoCouple);
+  }
+  temp = temp / 10;
+
+  return temp;
 }
